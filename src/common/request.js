ec97cf69ae7528ad1d8f04908a6d6037219bd4be
zhangzhaohuan <779637102@qq.com>
2018-07-25 10:32:32 +0800

    Update request.js

--- src/common/request.js
+++ src/common/request.js
@@ -1,5 +1,5 @@
 /**
- * create by yinghui(张兆欢) in 2017/5/29
+ * create by yinghui(张兆欢) in 2018/5/29
  * 
  */
 
@@ -8,8 +8,9 @@ import axios from 'axios';
 
 const WITH_CREDENTIALS = true;
 const CONTENT_TYPE = 'Content-Type';
-const MULTIPART = 'multipart/form-data';
-const URLENCODED = 'application/x-www-form-urlencoded';
+// const MULTIPART = 'multipart/form-data';
+// const URLENCODED = 'application/x-www-form-urlencoded';
+const URLENCODED = 'application/json;charset=UTF-8';
 const HEADEERS = { 'Accept': 'application/json', [CONTENT_TYPE]: URLENCODED };
 
 /**
@@ -20,28 +21,61 @@ const HEADEERS = { 'Accept': 'application/json', [CONTENT_TYPE]: URLENCODED };
  * @param {object} opts 请求选项
  */
 export async function request(method, url, params, opts) {
-    opts = Object.assign({}, opts);
-    params = Object.assign({}, params);
-    const headers = Object.assign({}, HEADEERS, opts.headers);
-    const withCredentials = opts.withCredentials || WITH_CREDENTIALS;
-    // if (opts.multipart) delete headers[CONTENT_TYPE];
-    let query, data, config;
-    config = {
-        "method": method,
-        "url": url,
-        "headers": headers,
-        "withCredentials": withCredentials
-    }
-    if (method == 'GET') {
-        query = querystring.stringify(params);
-        const url_query = JSON.stringify(params) == "{}" ? url : `${url}?${query}`;
-        config = Object.assign(config, { "params": params }, { "url": url_query });
-    } else {
-        config = Object.assign(config, { "data": params })
-    }
-    const res = await axios(config);
-    // const rs = await res.json();
+  opts = Object.assign({}, opts);
+  params = Object.assign({}, params);
+  const headers = Object.assign({}, HEADEERS, opts.headers);
+  const withCredentials = opts.withCredentials || WITH_CREDENTIALS;
+  // if (opts.multipart) delete headers[CONTENT_TYPE];
+  let query, config;
+  config = {
+    "method": method,
+    "url": url,
+    "headers": headers,
+    "withCredentials": withCredentials,
+    "timeout": 3000,
+  }
+  switch (method) {
+    case 'GET': query = querystring.stringify(params);
+                const url_query = JSON.stringify(params) === "{}" ? url : `${url}?${query}`;
+                config = Object.assign(config, { "params": params }); break;
+    case 'POST': config = Object.assign(config, { "data": params });  break;
+    case 'PUT': config = Object.assign(config, { "data": params });  break;    
+    case 'DELETE': config = Object.assign(config, { "data": params }); break;
+  }
+  axios.interceptors.request.use(function (config) {
+    //在请求发出之前进行一些操作
+    return config;
+  }, function (err) {
+    //Do something with request error
+    return Promise.reject(err);
+  });
+  axios.interceptors.response.use((response) => {
+    // Do something with response data
+    return response;
+  }, (err) => {
+    const res = JSON.parse(JSON.stringify(err));
+    // switch (res.response.status) {
+    //   case 400: err.message = '请求错误(400)' ; break;
+    //   case 401: err.message = '未授权，请重新登录(401)'; console.log('401');
+    //     break;
+    //   case 403: err.message = '拒绝访问(403)'; break;
+    //   case 404: err.message = '请求出错(404)'; break;
+    //   case 408: err.message = '请求超时(408)'; break;
+    //   case 500: err.message = '服务器错误(500)'; break;
+    //   case 501: err.message = '服务未实现(501)'; break;
+    //   case 502: err.message = '网络错误(502)'; break;
+    //   case 503: err.message = '服务不可用(503)'; break;
+    //   case 504: err.message = '网络超时(504)'; break;
+    //   case 505: err.message = 'HTTP版本不受支持(505)'; break;
+    // }
     return res;
+  });
+  try {
+    const res = await axios(config);
+    return res;
+  } catch (e) {
+    return {};
+  }
 }
 
 /**
@@ -49,7 +83,7 @@ export async function request(method, url, params, opts) {
  * @param {*} args 参数：url,param,opts
  */
 export function get(...args) {
-    return request('GET', ...args);
+  return request('GET', ...args);
 }
 
 /**
@@ -57,7 +91,23 @@ export function get(...args) {
  * @param {*} args 参数：url,param,opts
  */
 export function post(...args) {
-    return request('POST', ...args);
+  return request('POST', ...args);
+}
+
+/**
+ * 发起一个 put 请求
+ * @param {*} args 参数：url,param,opts
+ */
+export function put(...args) {
+  return request('PUT', ...args);
+}
+
+/**
+ * 发起一个 delete 请求
+ * @param {*} args 参数：url,param,opts
+ */
+export function deletes(...args) {
+  return request('DELETE', ...args);
 }
 
 
@@ -65,4 +115,6 @@ export function post(...args) {
 request.request = request;
 request.get = get;
 request.post = post;
+request.deletes = deletes;
+
 export default request;
