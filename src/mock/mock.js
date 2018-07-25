b1dc5f65c4f384d26f7b7ccb24a045e270d345c6
zhangzhaohuan <779637102@qq.com>
2018-07-25 10:29:52 +0800

    Update mock.js

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,1 +1,20 @@
 
+const db = require('./db.json');
+const currencies = require('./currencies.json');
+const address = require('./address.json');
+const loginHistory = require('./loginhistory.json');
+const messages = require('./messages.json');
+const orderlistAll = require('./orderlist-all.json');
+const apiList = require('./apiList.json');
+
+module.exports = function () {
+  return {
+    'db.json': db,
+    'currencies.json': currencies,
+    'address.json':address,
+    'loginhistory.json': loginHistory,
+    'messages.json': messages,
+    'orderlistAll.json': orderlistAll,
+    "apiList.json": apiList
+  }
+}
