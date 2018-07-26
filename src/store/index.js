e0b313a34774a3411899826dd1fea23308be4ab4
zhangzhaohuan <779637102@qq.com>
2018-07-26 16:21:27 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import todolist from './todolist';
 import test from './test';
-import intl from './intl'
 
 export default {
-  todolist:todolist,
-  test:test,
+  todolist: todolist,
+  test: test
 } 
